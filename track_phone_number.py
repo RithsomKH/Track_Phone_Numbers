0dc4dfc861fc411ee5658c7dfc68628bda90eb92
RithsomKH <111651248+RithsomKH@users.noreply.github.com>
2022-08-20 23:22:56 +0700

    import phonenumbers as ph  import os from phonenumbers import geocoder from phonenumbers import carrier while True:     os.system('cls')     _n=input("Enter_Number : ")     _city=ph.parse(_n,"KH")     print("Location  : "+geocoder.description_for_number(_city, "en"))     _con=ph.parse(_n,"KH")     print("SIM Card  : "+carrier.name_for_number(_con, "en"))     input()
    import phonenumbers as ph 
    import os
    from phonenumbers import geocoder
    from phonenumbers import carrier
    while True:
        os.system('cls')
        _n=input("Enter_Number : ")
        _city=ph.parse(_n,"KH")
        print("Location  : "+geocoder.description_for_number(_city, "en"))
        _con=ph.parse(_n,"KH")
        print("SIM Card  : "+carrier.name_for_number(_con, "en"))
        input()

---
+++ track_phone_number.py
@@ -0,0 +1,12 @@
+import phonenumbers as ph 
+import os
+from phonenumbers import geocoder
+from phonenumbers import carrier
+while True:
+    os.system('cls')
+    _n=input("Enter_Number : ")
+    _city=ph.parse(_n,"KH")
+    print("Location  : "+geocoder.description_for_number(_city, "en"))
+    _con=ph.parse(_n,"KH")
+    print("SIM Card  : "+carrier.name_for_number(_con, "en"))
+    input()
